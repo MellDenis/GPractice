38eeca54503ca3f636a5cebecb7815e8f1deaeb3
MellDenis <meldperalta@gmail.com>
2020-10-13 10:58:45 -0300

    Responsive

--- index.js
+++ index.js
@@ -1,22 +1,46 @@
+//Global Variables
 var actionItem = document.getElementById("txtAction");
 var buttonSubmit = document.getElementById("buttonSubmit");
 var list = document.getElementById("listItems");
 
+//Create a list with actions
 function createList() {
-
     var valueAction = actionItem.value;
     var content = document.createTextNode(valueAction);
-    var item = document.createElement('li');   
-    item.setAttribute('class','list-group-item');
-    item.appendChild(content);
-    list.appendChild(item); 
+    var item = document.createElement('li');
+    var numberItem = document.createElement('span');
+    var contentspan = document.createTextNode('1');
+    var bandera = item.value;
+    console.log(bandera);
+
+    if (valueAction != "") {
+        item.setAttribute('class', 'list-group-item d-flex justify-content-between align-items-center');
+        numberItem.setAttribute('class', 'badge badge-primary badge-pill');
+        numberItem.appendChild(contentspan);
+        item.appendChild(numberItem);
+        item.appendChild(content);
+        list.appendChild(item);
+        actionItem.value = "";
+    } else {
+        $('.alert').show();
+    }
 }
 
 //Create Event
 function divList() {
-
     buttonSubmit.addEventListener("click", createList());
-};
+}
 
-//Comprobar input vacío
- 
+//Edit items
+function editItems(id) {
+    alert('Editeded');
+}
+
+//Delete Items
+function deleteItems() {
+    var childItem = list.children.length;
+    console.log(childItem);
+    while (list.firstChild) {
+        list.removeChild(list.firstChild);
+    }
+};
