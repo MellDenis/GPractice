292b1c5e792496ddee33e1fdfdd8c938865d9297
MellDenis <meldperalta@gmail.com>
2020-10-15 06:07:03 -0300

    Adding features and style

---
+++ Readme.txt
@@ -0,0 +1,24 @@
+# Practicando JS :books:
+
+
+### Features 📋
+
+1. Crear un directorio con los archivos index.html e index.js y conectar el archivo js via tag <script>. 
+
+2. Crear via un <input> tipo texto y un <button> con sus respectivos atributos "ID", el id queda a discrecion de cada uno. 
+
+3. Crear un <div> vacio con el id de "listaAcciones" o algun nombre similar. 
+
+4. Capturar cada uno de estos elementos en sus respectivas variables en el archivo js. Utilizar nombres representativos para cada item. 
+
+5. Agregar un event listener al <button> con una funcion callback que se ocupe de adherir acciones al elemento "listaAcciones". 
+
+6. Agregar boton de eliminar Lista de Acciones. 
+
+7. Agregar boton de editar la lista de acciones. 
+
+8. Agregar boton de utilizar un item de la lista de acciones en un nuevo div "accionesActivas".
+
+9. Que la app sea Responsive.
+
+10.Razonamiento de estructura del proyecto.
--- index.js
+++ index.js
@@ -1,42 +1,121 @@
 //Global Variables
 var actionItem = document.getElementById("txtAction");
 var buttonSubmit = document.getElementById("buttonSubmit");
+var buttonDeleteAll = document.getElementById('buttonDeleteAll');
 var list = document.getElementById("listItems");
+var listActives = document.getElementById(listActives);
 
-//Create a list with actions
-function createList() {
-    var valueAction = actionItem.value;
-    var content = document.createTextNode(valueAction);
+
+//Create an action element
+function createNewElement(text) {
+
+    //Create an 'li'
     var item = document.createElement('li');
-    var numberItem = document.createElement('span');
-    var contentspan = document.createTextNode('1');
-    var bandera = item.value;
-    console.log(bandera);
+    //Value set by the user in the input
+    item.innerText = text;
+    //var content = document.createTextNode(valueAction);
+    //var itemsLi = list.getElementsByTagName('li');
+
+    //Create container for input and buttons
+    let divContainer = document.createElement('div');
+    //Set atributes
+    divContainer.classList.add('input-group');
+    let divInside = document.createElement('div');
+    divInside.classList.add('input-group-append');
+
+    //Create an input 
+    let inputActionList = document.createElement('input');
+    //Set attribute
+    inputActionList.setAttribute('type', 'text');
+    inputActionList.classList.add('form-control');
+
+    //Create the 3 buttons
+    let buttonActive = document.createElement('button');
+    buttonActive.className = 'btn btn-outline-success';
+    buttonActive.innerText = 'ACTIVE';
+
+    //Add eventListener for active
+    buttonActive.addEventListener('click', sendToActiveList);
+
+
+    let buttonEdit = document.createElement('button');
+    buttonEdit.className = 'btn btn-outline-warning';
+    buttonEdit.innerText = 'EDIT';
+
+    //Add eventListener for edit
+    //buttonEdit.addEventListener('click', editInfoInput);
+
+    let buttonDelete = document.createElement('button');
+    buttonDelete.className = 'btn btn-outline-danger';
+    buttonDelete.innerText = 'DELETE';
+
+    //Add eventListener for delete
+    buttonDelete.addEventListener('click',deleteItem);
+
+    //Append the buttons and input to the div
+    divInside.appendChild(inputActionList);
+    divInside.appendChild(buttonActive);
+    divInside.appendChild(buttonEdit);
+    divInside.appendChild(buttonDelete);
+
+    //Append the Divs
+    divContainer.appendChild(divInside);
+
+    //Append the div to the li
+    item.appendChild(divContainer);
+
+    //Append the 'li' to the 'ul'
+    //list.insertBefore(item,list.childNodes[0]);
+    list.appendChild(item);
+}
+
+//Adding to the list the action
+function addItemToList() {
+    var valueAction = actionItem.value;
 
     if (valueAction != "") {
-        item.setAttribute('class', 'list-group-item d-flex justify-content-between align-items-center');
-        numberItem.setAttribute('class', 'badge badge-primary badge-pill');
-        numberItem.appendChild(contentspan);
-        item.appendChild(numberItem);
-        item.appendChild(content);
-        list.appendChild(item);
+        createNewElement(valueAction);
         actionItem.value = "";
     } else {
         $('.alert').show();
     }
 }
 
-//Create Event
+//Create Add Event
 function divList() {
-    buttonSubmit.addEventListener("click", createList());
+    buttonSubmit.addEventListener("click", addItemToList());
 }
 
-//Edit items
-function editItems(id) {
-    alert('Editeded');
+//Send to Active List
+function sendToActiveList() {
+    //'li' child
+    let itemChild = this.parentNode.parentNode.parentNode;
+    //'ul' parent
+    let parent = itemChild.parentNode;
+    //'id' parent
+    let id = parent.id;
+    console.log(itemChild);
+    console.log("IdPadre: " + id);
+    //Check if the item should go in the 'listActives' or re-added to 'listItems'
+    let indice = (id === "listItems") ? document.getElementById("listActives") : document.getElementById("listItems");
+    //Remove the 'li' item
+    parent.removeChild(itemChild);
+    //Add to the new 'ul'
+    indice.insertBefore(itemChild,indice.childNodes[0])
+
 }
 
-//Delete Items
+//Remove item by one
+function deleteItem() {
+    //Searching the childItem
+    let itemChild = this.parentNode.parentNode.parentNode;
+    //Searching the parentItem
+    let parent = itemChild.parentNode;
+    //Removing the Item from the list
+    parent.removeChild(itemChild);
+}
+
+//Delete all items
 function deleteItems() {
     var childItem = list.children.length;
     console.log(childItem);
@@ -44,3 +123,8 @@ function deleteItems() {
         list.removeChild(list.firstChild);
     }
 };
+
+//Create Delete Event
+function deleteAll() {
+    buttonDeleteAll.addEventListener('click',deleteItems());
+}
